0ae5153a0ae0ea1baac8ef71bd7788b2f22815b4
Kevin Jones <nawforce@gmail.com>
2017-02-11 15:03:18 +0000

    Allow for missing labels file

--- src/main/scala/io/github/nahforce/apexlink/metadata/LabelReader.scala
+++ src/main/scala/io/github/nahforce/apexlink/metadata/LabelReader.scala
@@ -12,17 +12,19 @@ class LabelReader extends SymbolReader {
     try {
       val labelsFile = ctx.getBaseDir.resolve("labels").resolve("CustomLabels.labels")
       LinkerLog.pushContext(labelsFile.toString)
-      LinkerLog.ifNotLogAndThrow(Files.isRegularFile(labelsFile), 0, "Labels file is not present")
-      LinkerLog.ifNotLogAndThrow(Files.isReadable(labelsFile), 0, "Labels file is not readable")
+      if (Files.exists(labelsFile)) {
+        LinkerLog.ifNotLogAndThrow(Files.isRegularFile(labelsFile), 0, "Labels file is not a regular file")
+        LinkerLog.ifNotLogAndThrow(Files.isReadable(labelsFile), 0, "Labels file is not readable")
 
-      val root = XMLLineLoader.loadFile(labelsFile.toString)
-      XMLUtils.ifNotElemLogAndThrow(root, "CustomLabels")
+        val root = XMLLineLoader.loadFile(labelsFile.toString)
+        XMLUtils.ifNotElemLogAndThrow(root, "CustomLabels")
 
-      root.child.foreach {
-        case elem: Elem =>
-          XMLUtils.ifNotElemLogAndThrow(elem, "labels")
-          Label.create(elem).foreach(l => ctx.addLabel(l))
-        case _ =>
+        root.child.foreach {
+          case elem: Elem =>
+            XMLUtils.ifNotElemLogAndThrow(elem, "labels")
+            Label.create(elem).foreach(l => ctx.addLabel(l))
+          case _ =>
+        }
       }
     } catch {
       case _: LinkerException => () // Ignore, just used to abort processing
