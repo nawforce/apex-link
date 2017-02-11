e65fa4906adfdeda41d3855470461001e9cc9b23
Kevin Jones <nawforce@gmail.com>
2017-02-11 15:58:42 +0000

    Add more samples for testing

--- src/test/scala/io/github/nahforce/apexlink/SampleTest.scala
+++ src/test/scala/io/github/nahforce/apexlink/SampleTest.scala
@@ -20,4 +20,35 @@ class SampleTest {
     sample("samples/SalesforceFoundation/Cumulus/src")
   }
 
+  @Test def sample4 : Unit = {
+    sample("samples/SalesforceFoundation/HEDAP/src")
+  }
+
+  @Test def sample5 : Unit = {
+    sample("samples/SalesforceFoundation/CampaignTools/src")
+  }
+
+  @Test def sample6 : Unit = {
+    sample("samples/SalesforceFoundation/Volunteers-for-Salesforce/src")
+  }
+
+  @Test def sample7 : Unit = {
+    sample("samples/SalesforceFoundation/Relationships/src")
+  }
+
+  @Test def sample8 : Unit = {
+    sample("samples/SalesforceFoundation/Households/src")
+  }
+
+  @Test def sample9 : Unit = {
+    sample("samples/SalesforceFoundation/Recurring_Donations/src")
+  }
+
+  @Test def sample10 : Unit = {
+    sample("samples/SalesforceFoundation/Contacts_and_Organizations/src")
+  }
+
+  @Test def sample11 : Unit = {
+    sample("samples/SalesforceFoundation/Affiliations/src")
+  }
 }
