eab66105ea576a577b603b5851b7fe9280d908e8
Kevin Jones <nawforce@gmail.com>
2017-02-11 15:16:24 +0000

    Squashed 'samples/SalesforceFoundation/HEDAP/' content from commit 5bcd97d
    git-subtree-dir: samples/SalesforceFoundation/HEDAP
    git-subtree-split: 5bcd97da9cb75624358a313943365c653a98b79a

---
+++ src/aura/STG_CMP_Affl/STG_CMP_AfflController.js
@@ -0,0 +1,53 @@
+({
+	init : function(component, event, helper) {
+		helper.init(component);
+	},
+
+	setAfflProgEnrollDel : function(component, event, helper) {
+		helper.setAfflProgEnrollDel(component, event);
+	},
+
+	settsLinkClicked : function(component, event, helper) {
+		helper.settsLinkClicked(component);
+	},
+
+	mappingsLinkClicked : function(component, event, helper) {
+		helper.mappingsLinkClicked(component);
+	},
+
+	toggleIsView : function(component, event) {
+		component.set("v.isView", event.getParam("isView"));
+	},
+
+	saveMappings : function(component, event, helper) {
+		helper.saveMappings(component);
+	},
+
+	resetSettings : function(component, event, helper) {
+		helper.resetSettings(component);
+	},
+
+	newAfflMapping : function(component, event, helper) {
+		helper.newAfflMapping(component);
+	},
+
+	deleteAfflMappingRow : function(component, event, helper) {
+		if (confirm("Are you sure you want to delete this row?")) {
+			var id = event.getParam("id");
+			var position = event.getParam("position");
+			helper.deleteAfflMappingRow(component, id, position);
+		}
+	},
+
+	newAfflMappingKeyup : function (component, event) {
+		// accRecType && primaryField are  required fields
+		var accRecType = component.find("accRecType").get("v.value") || "";
+		var primaryField = component.find("primaryField").get("v.value") || "";
+
+		if( accRecType.length > 0 && primaryField.length > 0  ) { 
+			component.find("newAfflMappingBtn").set("v.disabled", false);
+		} else {
+			component.find("newAfflMappingBtn").set("v.disabled", true);	
+		}
+	},
+})
