fce4162606fc3082f355314e689259fb82ccb3bd
Kevin Jones <nawforce@gmail.com>
2019-09-04 21:25:59 +0100

    Handle ghosted types on SchemaManager

--- src/main/scala/com/nawforce/types/SchemaManager.scala
+++ src/main/scala/com/nawforce/types/SchemaManager.scala
@@ -1,3 +1,30 @@
+/*
+ [The "BSD licence"]
+ Copyright (c) 2019 Kevin Jones
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+ 3. The name of the author may not be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package com.nawforce.types
 
 import com.nawforce.utils.{DotName, Name}
@@ -20,15 +47,24 @@ class SchemaSObjectType(pkg: PackageDeclaration) extends NamedTypeDeclaration(Ty
     if (!staticOnly)
       return None
 
+    val demangled = DotName(name).demangled
+    if (demangled.isCompound && pkg.basePackages().filter(_.isGhosted).exists(_.namespace == demangled.firstName)) {
+      return Some(createField(name))
+    }
+
     sobjectFields.get(name).orElse({
       val td = pkg.getType(DotName(name))
       if (td.nonEmpty && td.get.superClassDeclaration.exists(superClass => superClass.typeName == TypeName.SObject)) {
-        val fd = CustomFieldDeclaration(name, TypeName.DescribeSObjectResult, asStatic = true)
-        sobjectFields.put(name, fd)
-        Some(fd)
+        Some(createField(name))
       } else {
         None
       }
     })
+  }
+
+  private def createField(name:Name): FieldDeclaration = {
+    val fd = CustomFieldDeclaration(name, TypeName.DescribeSObjectResult, asStatic = true)
+    sobjectFields.put(name, fd)
+    fd
   }
 }
---
+++ src/test/scala/com/nawforce/types/SchemaManagerTest.scala
@@ -0,0 +1,63 @@
+package com.nawforce.types
+
+import java.nio.file.Files
+
+import com.google.common.jimfs.{Configuration, Jimfs}
+import com.nawforce.api.Org
+import com.nawforce.utils.Name
+import org.scalatest.FunSuite
+
+class SchemaManagerTest extends FunSuite {
+
+  def customObject(label: String, fields: Seq[(String, String, Option[String])]): String = {
+    val fieldMetadata = fields.map(field => {
+      s"""
+         |    <fields>
+         |        <fullName>${field._1}</fullName>
+         |        <type>${field._2}</type>
+         |        ${if (field._3.nonEmpty) s"<referenceTo>field._3.get</referenceTo>" else ""}
+         |    </fields>
+         |""".stripMargin
+    })
+
+    s"""<?xml version="1.0" encoding="UTF-8"?>
+       |<CustomObject xmlns="http://soap.sforce.com/2006/04/metadata">
+       |    <fullName>$label</fullName>
+       |    $fieldMetadata
+       |</CustomObject>
+       |""".stripMargin
+  }
+
+  test("Standard object visible") {
+    val fs = Jimfs.newFileSystem(Configuration.unix)
+    Files.write(fs.getPath("Dummy.cls"),"public class Dummy { {DescribeSObjectResult r = SObjectType.Account;} }".getBytes())
+
+    val org = new Org()
+    val pkg = org.addPackageInternal(Name.Empty, Seq(fs.getPath("/")), Seq())
+    pkg.deployAll()
+    assert(!org.issues.hasMessages)
+  }
+
+  test("Custom object visible") {
+    val fs = Jimfs.newFileSystem(Configuration.unix)
+    Files.write(fs.getPath("Foo__c.object"), customObject("Foo", Seq(("Bar__c", "Text", None))).getBytes())
+    Files.write(fs.getPath("Dummy.cls"),"public class Dummy { {DescribeSObjectResult r = SObjectType.Foo__c;} }".getBytes())
+
+    val org = new Org()
+    val pkg = org.addPackageInternal(Name.Empty, Seq(fs.getPath("/")), Seq())
+    pkg.deployAll()
+    assert(!org.issues.hasMessages)
+  }
+
+  test("Ghosted custom object visible") {
+    val fs = Jimfs.newFileSystem(Configuration.unix)
+    Files.write(fs.getPath("Dummy.cls"),"public class Dummy { {DescribeSObjectResult r = SObjectType.ghosted__Foo__c;} }".getBytes())
+
+    val org = new Org()
+    val ghosted = org.addPackageInternal(Name("ghosted"), Seq(), Seq())
+    val pkg = org.addPackageInternal(Name.Empty, Seq(fs.getPath("/")), Seq(ghosted))
+    pkg.deployAll()
+    org.issues.dumpMessages(false)
+    assert(!org.issues.hasMessages)
+  }
+}
