931f6c228ed18253bdda0e0d247f41ba451763cf
Kevin Jones <nawforce@gmail.com>
2019-11-26 21:21:22 +0000

    Cut out tool support to slim package

--- cli/package.sh
+++ cli/package.sh
@@ -3,19 +3,7 @@
 rm -rf jars
 mkdir jars
 cp ../target/apexlink-0.5.jar jars/.
-cp ../target/dependency/ST4-*.jar jars/.
-cp ../target/dependency/antlr-*.jar jars/.
-cp ../target/dependency/antlr-runtime-*.jar jars/.
 cp ../target/dependency/antlr4-runtime-*.jar jars/.
-cp ../target/dependency/commons-beanutils-*.jar jars/.
-cp ../target/dependency/commons-logging-*.jar jars/.
-cp ../target/dependency/force-apex-api-*.jar jars/.
-cp ../target/dependency/force-metadata-api-*.jar jars/.
-cp ../target/dependency/force-partner-api-*.jar jars/.
-cp ../target/dependency/force-wsc-*.jar jars/.
-cp ../target/dependency/jackson-core-asl-*.jar jars/.
-cp ../target/dependency/jackson-mapper-asl-*.jar jars/.
-cp ../target/dependency/lift-json_*.jar jars/.
 cp ../target/dependency/logback-classic-*.jar jars/.
 cp ../target/dependency/logback-core-*.jar jars/.
 cp ../target/dependency/scala-library-*.jar jars/.
@@ -24,4 +12,4 @@ cp ../target/dependency/scala-reflect-*.jar jars/.
 cp ../target/dependency/scala-xml_*.jar jars/.
 cp ../target/dependency/scalaz-core_*.jar jars/.
 cp ../target/dependency/slf4j-api-*.jar jars/.
-cp ../target/dependency/stringtemplate-*.jar jars/.
+cp ../target/dependency/lift-json_*.jar jars/.
