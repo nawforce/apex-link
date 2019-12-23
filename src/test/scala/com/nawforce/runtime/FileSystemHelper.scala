86e99b056660144be4c4d3d5df57329737d2f87a
Kevin Jones <nawforce@gmail.com>
2019-12-23 20:56:08 +0000

    Add missed files

---
+++ src/test/scala/com/nawforce/runtime/FileSystemHelper.scala
@@ -0,0 +1,25 @@
+package com.nawforce.runtime
+
+import java.nio.file.Files
+
+import com.google.common.jimfs.{Configuration, Jimfs}
+import com.nawforce.common.path.PathLike
+import com.nawforce.runtime.path.Path
+
+object FileSystemHelper {
+
+  // Abstract virtual filesystem for testing
+  def run[T](files: Map[String, String])(verify: PathLike => T): T = {
+    val config = Configuration.unix().toBuilder
+      .setWorkingDirectory("/")
+      .build()
+    val fs = Jimfs.newFileSystem(config)
+    val rootDir = fs.getRootDirectories.iterator().next()
+    files.foreach(kv => {
+      val path = rootDir.resolve(kv._1)
+      Files.createDirectories(path.getParent)
+      Files.write(path, kv._2.getBytes())
+    })
+    verify(Path(rootDir))
+  }
+}
