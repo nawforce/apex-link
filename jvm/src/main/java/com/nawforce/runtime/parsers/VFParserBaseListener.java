a5d6d05e206b414dccfdcaab4b8ea31cb2805396
Kevin Jones <nawforce@gmail.com>
2021-02-27 15:34:44 +0000

    Remove doc comments causing compiler warnings (again)

--- jvm/src/main/java/com/nawforce/runtime/parsers/VFParserBaseListener.java
+++ jvm/src/main/java/com/nawforce/runtime/parsers/VFParserBaseListener.java
@@ -120,28 +120,8 @@ public class VFParserBaseListener implements VFParserListener {
 	 */
 	@Override public void exitScriptChardata(VFParser.ScriptChardataContext ctx) { }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
 	@Override public void enterEveryRule(ParserRuleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
 	@Override public void exitEveryRule(ParserRuleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
 	@Override public void visitTerminal(TerminalNode node) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
 	@Override public void visitErrorNode(ErrorNode node) { }
 }
