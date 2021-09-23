e860e83290d313e49e5bd1b2d330a87876068d8c
Baizel Mathew <bmathew@financialforce.com>
2021-09-23 16:46:50 +0100

    Add unit tests

--- src/test/scala/com/nawforce/apexlink/pkg/CompletionProviderTest.scala
+++ src/test/scala/com/nawforce/apexlink/pkg/CompletionProviderTest.scala
@@ -1,19 +1,79 @@
 package com.nawforce.apexlink.pkg
 
+import com.nawforce.apexlink.rpc.CompletionItemLink
 import com.nawforce.apexlink.{FileSystemHelper, TestHelper}
 import com.nawforce.pkgforce.path.PathLike
 import org.scalatest.funsuite.AnyFunSuite
 
 class CompletionProviderTest extends AnyFunSuite with TestHelper {
 
-  test("Outer class match") {
-    FileSystemHelper.run(Map("Dummy.cls" -> "public class Dummy {}")) { root: PathLike =>
-      val org = createHappyOrg(root)
-      val path = root.join("Dummy.cls")
+  test("Method class match") {
+    FileSystemHelper.run(Map(
+      "Dummy.cls" -> "public class Dummy {public String methodA(){} public String methodB(String a, String b){} }")) {
+      root: PathLike =>
+        val org = createOrg(root)
+        val path = root.join("Completion.cls")
+        val content = "public class Completion { public Completion() {String a = new Dummy().me"
+        assert(
+          org
+            .getCompletions(path.toString, line = 1, offset = content.length, content)
+            .sameElements(
+              Array(CompletionItemLink("methodB(a, b)", "Method"), CompletionItemLink("methodA()", "Method"))))
+    }
+  }
+
+  test("Instance Field completions") {
+    FileSystemHelper.run(
+      Map("Dummy.cls" -> "public class Dummy { public static String FieldS = ''; public string fieldA = '';}")) {
+      root: PathLike =>
+        val org = createOrg(root)
+        val path = root.join("Completion.cls")
+        val content = "public class Completion { public Completion() {String a = new Dummy().fi"
+        assert(
+          org
+            .getCompletions(path.toString, line = 1, offset = content.length, content)
+            .sameElements(Array(CompletionItemLink("fieldA", "Field"))))
+    }
+  }
+
+  test("Static Field completions") {
+    FileSystemHelper.run(
+      Map("Dummy.cls" -> "public class Dummy { public static String fieldS = ''; public string fieldA = '';}")) {
+      root: PathLike =>
+        val org = createOrg(root)
+        val path = root.join("Completion.cls")
+        val content = "public class Completion { public Completion() {String a = Dummy.fi"
+        assert(
+          org
+            .getCompletions(path.toString, line = 1, offset = content.length, content)
+            .sameElements(Array(CompletionItemLink("fieldS", "Field") )))
+    }
+  }
+
+  test("Inner class completion") {
+    FileSystemHelper.run(Map("Dummy.cls" -> "public class Dummy {class Inner{} }")) { root: PathLike =>
+      val org = createOrg(root)
+      val path = root.join("Completion.cls")
+      val content = "public class Completion { public Completion() {Dummy.Inn"
       assert(
-        org.unmanaged
-          .getCompletions(path, line = 1, offset = 14, None)
-          .isEmpty)
+        org
+          .getCompletions(path.toString, line = 1, offset = content.length, content)
+          .sameElements(Array(CompletionItemLink("Inner", "TypeParameter"))))
+    }
+  }
+
+  test("Instance method from static") {
+    //TODO: fix this for expressions
+    FileSystemHelper.run(
+      Map("Foo.cls" -> "public class Foo { public String method(){}}",
+          "Baz.cls" -> "public class Baz { public static Foo bar = new Foo();}")) { root: PathLike =>
+      val org = createOrg(root)
+      val path = root.join("Dummy.cls")
+      val content = "public class Dummy { public String method(){Baz.bar.me"
+      assert(
+        org
+          .getCompletions(path.toString, line = 1, offset = content.length, content)
+          .sameElements(Array(CompletionItemLink("method()", "Method"))))
     }
   }
 
