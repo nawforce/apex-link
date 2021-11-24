1a714667bfc5d0dd31bf13cabd77bfc08da6af43
Baizel Mathew <bmathew@financialforce.com>
2021-11-24 15:48:11 +0000

    Fix bug where the shortest path was ignored when traversing the graph Filter out ignored nodes in uses, extends and implements

--- src/main/scala/com/nawforce/apexlink/deps/DownWalker.scala
+++ src/main/scala/com/nawforce/apexlink/deps/DownWalker.scala
@@ -45,11 +45,13 @@ class DownWalker(org: Org, apexOnly: Boolean) {
    */
   def walk(identifiers: Array[TypeIdentifier], depth: Int, ignoring:Array[TypeIdentifier]): Array[NodeData] = {
     val collectedNodes = new ArrayBuffer[NodeData]()
-    val collectedIds = new mutable.HashSet[TypeIdentifier]()
+    val collectedIds   = new mutable.HashMap[TypeIdentifier, Int]()
 
-    val roots = identifiers.filterNot(i=>ignoring.contains(i)).flatMap(i => createNode(i,ignoring))
+    val roots = identifiers
+      .filterNot(i => ignoring.contains(i))
+      .flatMap(i => createNode(i,ignoring))
     collectedNodes.addAll(roots)
-    collectedIds.addAll(identifiers)
+    identifiers.foreach(i => collectedIds.addOne(i, 0))
 
     roots.foreach(root => {
       walkNode(root, collectedNodes, collectedIds, depth, ignoring)
@@ -59,29 +61,39 @@ class DownWalker(org: Org, apexOnly: Boolean) {
 
   private def walkNode(node: NodeData,
                        collector: ArrayBuffer[NodeData],
-                       collected: mutable.Set[TypeIdentifier],
+                       collected: mutable.Map[TypeIdentifier,Int],
                        depth: Int,
                        ignoring:Array[TypeIdentifier]): Unit = {
     if (depth == 0) return
-    (node.extending ++ node.implementing ++ node.using).filterNot(n=>ignoring.contains(n)).foreach(id => {
-      if (!collected.contains(id)) {
-        collected.add(id)
-        createNode(id, ignoring).foreach(node => {
-          collector.append(node)
-          walkNode(node, collector, collected, depth - 1, ignoring)
-        })
-      }
-    })
+    (node.extending ++ node.implementing ++ node.using)
+      .filterNot(n => ignoring.contains(n))
+      .foreach(id => {
+        if (!collected.contains(id) || collected(id) <= depth) {
+          collected.addOne(id, depth)
+          createNode(id, ignoring).foreach(node => {
+            findAndReplace(collector, node)
+            walkNode(node, collector, collected, depth - 1, ignoring)
+          })
+        }
+      })
   }
 
-  private def createNode(id: TypeIdentifier, ignoring:Array[TypeIdentifier]): Option[NodeData] = {
+  private def findAndReplace(collector: ArrayBuffer[NodeData], nodeData: NodeData): Unit = {
+    val index = collector.indexWhere(n => n.id.equals(nodeData.id))
+    if (index >= 0)
+      collector(index) = nodeData
+    else
+      collector.addOne(nodeData)
+  }
+
+  private def createNode(id: TypeIdentifier, ignoring: Array[TypeIdentifier]): Option[NodeData] = {
     org
       .asInstanceOf[OrgImpl]
       .findTypeIdentifier(id)
       .filter(td => !apexOnly || td.isInstanceOf[ApexDeclaration])
       .collect { case td: DependencyHolder => td }
       .map(td => {
-        val pkg = td.moduleDeclaration.map(_.pkg)
+        val pkg    = td.moduleDeclaration.map(_.pkg)
         val typeId = pkg.map(pkg => TypeIdentifier(pkg.namespace, td.typeName))
 
         val inherits =
@@ -91,10 +103,11 @@ class DownWalker(org: Org, apexOnly: Boolean) {
               case _                        => None
             })
             .filterNot(d => typeId.contains(d._2))
+            .filterNot(d => ignoring.contains(d._2))
             .toSet
-        val extending = inherits.filter(id => id._1 == CLASS_NATURE).map(_._2)
+        val extending    = inherits.filter(id => id._1 == CLASS_NATURE).map(_._2)
         val implementing = inherits.filter(id => id._1 == INTERFACE_NATURE).map(_._2)
-        val output = extending ++ implementing
+        val output       = extending ++ implementing
 
         val all =
           pkg
@@ -102,9 +115,17 @@ class DownWalker(org: Org, apexOnly: Boolean) {
               Option(pkg.getDependencies(id, outerInheritanceOnly = false, apexOnly))
             })
             .getOrElse(Array[TypeIdentifier]())
-        val uses = all.filterNot(output.contains).filterNot(id => typeId.contains(id))
-
-        NodeData(id, td.nature.value, transitiveCollector.count(id, ignoring), extending.toArray, implementing.toArray, uses)
+        val uses = all.filterNot(output.contains)
+                      .filterNot(id => typeId.contains(id))
+                      .filterNot(id => ignoring.contains(id))
+        NodeData(
+          id,
+          td.nature.value,
+          transitiveCollector.count(id, ignoring),
+          extending.toArray,
+          implementing.toArray,
+          uses
+        )
       })
   }
 }
