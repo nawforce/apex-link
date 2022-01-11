24a08f32c2c2fa8ae40d700980c799f0c16e18fe
Baizel Mathew <bmathew@financialforce.com>
2022-01-11 12:18:56 +0000

    Add logic to provide definitions for triggers

--- src/main/scala/com/nawforce/apexlink/org/DefinitionProvider.scala
+++ src/main/scala/com/nawforce/apexlink/org/DefinitionProvider.scala
@@ -15,8 +15,8 @@ package com.nawforce.apexlink.org
 
 import com.nawforce.apexlink.org.TextOps.TestOpsUtils
 import com.nawforce.apexlink.rpc.LocationLink
-import com.nawforce.apexlink.types.apex.FullDeclaration
-import com.nawforce.pkgforce.documents.{ApexClassDocument, MetadataDocument}
+import com.nawforce.apexlink.types.apex.{ApexFullDeclaration, FullDeclaration, TriggerDeclaration}
+import com.nawforce.pkgforce.documents.{ApexClassDocument, ApexTriggerDocument, MetadataDocument}
 import com.nawforce.pkgforce.path.{IdLocatable, Locatable, PathLike, UnsafeLocatable}
 
 trait DefinitionProvider {
@@ -45,7 +45,7 @@ trait DefinitionProvider {
     }).toArray
   }
 
-  private def loadSourceAndType(path: PathLike, content: Option[String]): Option[(String, FullDeclaration)] = {
+  private def loadSourceAndType(path: PathLike, content: Option[String]): Option[(String, ApexFullDeclaration)] = {
     // We need source code no matter what
     val sourceOpt = content.orElse(path.read().toOption)
     if (sourceOpt.isEmpty)
@@ -53,24 +53,35 @@ trait DefinitionProvider {
 
     // If we don't have new source we can assume the loaded type is current, but it could be a summary
     if (content.isEmpty) {
-      MetadataDocument(path)
-        .collect { case doc: ApexClassDocument => doc }
-        .flatMap(doc => orderedModules.view.flatMap(_.moduleType(doc.typeName(namespace))).headOption)
-        .collect { case td: FullDeclaration => td }
-        .orElse({
-          // If not try and load a temp version to work with, this is expensive
-          loadClass(path, sourceOpt.get)._2
-        })
-        .map(td => (sourceOpt.get, td))
+      MetadataDocument(path) collect {
+        case doc: ApexTriggerDocument =>
+          orderedModules.view.flatMap(_.moduleType(doc.typeName(namespace))).headOption
+            .collect { case td: TriggerDeclaration => td }
+            .orElse({
+              loadTrigger(path, sourceOpt.get)._2
+            })
+            .map(td => (sourceOpt.get, td)).get
+        case doc: ApexClassDocument =>
+          orderedModules.view.flatMap(_.moduleType(doc.typeName(namespace))).headOption
+            .collect { case td: FullDeclaration => td }
+            .orElse({
+              loadClass(path, sourceOpt.get)._2
+            })
+            .map(td => (sourceOpt.get, td)).get
+      }
     } else {
       // No option but to load it as content is being provided
-      loadClass(path, sourceOpt.get)._2.map(td => (sourceOpt.get, td))
+      if (path.basename.toLowerCase.endsWith(".trigger")) {
+        loadTrigger(path, sourceOpt.get)._2.map(td => (sourceOpt.get, td))
+      } else {
+        loadClass(path, sourceOpt.get)._2.map(td => (sourceOpt.get, td))
+      }
     }
   }
 
   /** Extract a location link from an expression at the passed location */
-  private def locateFromValidation(td: FullDeclaration, line: Int, offset: Int): Option[LocationLink] = {
-    val resultMap = td.getBodyDeclarationValidationMap(line, offset)
+  private def locateFromValidation(td: ApexFullDeclaration, line: Int, offset: Int): Option[LocationLink] = {
+    val resultMap = td.getValidationMap(line, offset)
 
     // Find the inner-most expression containing location from those that do
     val exprLocations = resultMap.keys.filter(_.contains(line, offset))
